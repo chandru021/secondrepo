b20bef1eb6a0fa9644dfeae63fab9c22dbcf9563
Chandru <delhichandru@outlook.com>
2022-04-14 16:43:43 +0530

    This change was made in the factorial branch

--- secondpy.py
+++ secondpy.py
@@ -2,4 +2,5 @@ print("pushing changes on first line")
 print("conflict resolved")
 print("so far so good")
 print("triying to do it again")
+#this was added in the factorial branch
 print("this change was made in the website")
